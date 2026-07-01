1e45e1fc07da5ded7ee7544818deb417a837342c
ManifestPeace <MANIFESTCOACHINGLLC@GMAIL.COM>
2026-07-01 00:11:14 -0400

    Update upcomingTraining.js

--- src/config/upcomingTraining.js
+++ src/config/upcomingTraining.js
@@ -47,7 +47,7 @@ export const upcomingTraining = {
       ],
 
       when: "9/29/26 - 10/01/26 8:30am- 5:00pm (3days)",       //  e.g. "March 14–16, 2026"
-      where: "Acworth, GA (< 60min from Metro Atlanta, GA)",   //  e.g. "Atlanta, GA · In person"
+      where: "Acworth, GA (< 30 min from Atlanta, GA)",   //  e.g. "Atlanta, GA · In person"
       trainer: "Ayana F. McKanney, LCSW — Founder of The Peace Practice",
       cost: "$1,700",
       website: "",
@@ -76,7 +76,7 @@ export const upcomingTraining = {
       ],
 
       when: "Friday- 10/02/2026-9:00am-3:00pm",
-      where: "Acworth, GA (< 60min from Metro Atlanta, GA)",
+      where: "Acworth, GA (< 30 min from Atlanta, GA)",
       trainer: "Ayana F. McKanney, LCSW — Founder of The Peace Practice",
       cost: "$199",
       website: "",                          //  leave "" to hide the website link
