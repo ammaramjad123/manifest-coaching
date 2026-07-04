6e9da2a53bcb341e273321962ac20d7ee4cd729f
ManifestPeace <MANIFESTCOACHINGLLC@GMAIL.COM>
2026-07-03 21:11:55 -0400

    Update upcomingTraining.js

--- src/config/upcomingTraining.js
+++ src/config/upcomingTraining.js
@@ -79,7 +79,7 @@ export const upcomingTraining = {
       where: "Acworth, GA (< 30 min from Atlanta, GA)",
       trainer: "Ayana F. McKanney, LCSW — Founder of The Peace Practice",
       cost: "$199",
-      website: "",                          //  leave "" to hide the website link
+      website: "https://acceleratedresolutiontherapy.com/reg/enroll.php?class=SAF-T1+10%2F02%2F26+ACCGA&savesrch=%2Freg%2Ficat.php%3Fcourse%3DSAF-T1",                          //  leave "" to hide the website link
 
       ctaText: "Reserve Your Spot",
       ctaLink: "https://calendly.com/manifestcoachingllc/clarity-call",
