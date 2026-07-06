cfeb086657f01f9be2298cd6c3640e94291e865d
ManifestPeace <MANIFESTCOACHINGLLC@GMAIL.COM>
2026-07-06 14:49:11 -0400

    Update siteImages.js

--- src/config/siteImages.js
+++ src/config/siteImages.js
@@ -167,7 +167,7 @@ export const images = {
   // ───────────── The PIVOT-IMMIGRATION EVALUATIONS PAGE ─────────────
   immigration: {
     card1: "/new/global-residence-index-wOj5HTw2YMc-unsplash.jpg",    // 1st service card image
-    card2: "/new/markus-winkler-aXkQK_mc5Dw-unsplash.jpg",   // 2nd service card image
+    card2: "/new/surface-CqQWt4a-XfY-unsplash.jpg,   // 2nd service card image
     showcase: "/new/marina-yalanska-kt8Qn_2U9r8-unsplash.jpg",  // Large banner photo
     cta: "/new/david-clode-13PjNBaDMcg-unsplash.jpg",     // Bottom call-to-action background
   },
