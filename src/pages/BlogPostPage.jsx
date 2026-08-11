b8dda14c2b9a11d62b67a0b27905803f1a5a4b1b
Trexhmza <iamhamzaaaaa@gmail.com>
2026-08-11 23:49:04 +0500

    SEO: each blog post now shares its own hero image via OG/Twitter tags

--- src/pages/BlogPostPage.jsx
+++ src/pages/BlogPostPage.jsx
@@ -11,6 +11,7 @@ export default function BlogPostPage() {
   }
 
   const url = `https://www.thepeacepractice.me/blog/${post.slug}`;
+  const heroImageUrl = `https://www.thepeacepractice.me${post.heroImage}`;
 
   const schema = {
     "@context": "https://schema.org",
@@ -44,6 +45,23 @@ export default function BlogPostPage() {
       <title>{post.metaTitle}</title>
       <meta name="description" content={post.metaDescription} />
       <link rel="canonical" href={url} />
+      <meta property="og:type" content="article" />
+      <meta property="og:site_name" content="The Peace Practice" />
+      <meta property="og:title" content={post.metaTitle} />
+      <meta property="og:description" content={post.metaDescription} />
+      <meta property="og:url" content={url} />
+      <meta property="og:image" content={heroImageUrl} />
+      <meta property="og:image:secure_url" content={heroImageUrl} />
+      <meta property="og:image:alt" content={post.heroImageAlt} />
+      <meta property="og:locale" content="en_US" />
+      <meta property="article:published_time" content={post.publishDate} />
+      <meta property="article:modified_time" content={post.updatedDate} />
+      <meta property="article:author" content={post.author} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={post.metaTitle} />
+      <meta name="twitter:description" content={post.metaDescription} />
+      <meta name="twitter:image" content={heroImageUrl} />
+      <meta name="twitter:image:alt" content={post.heroImageAlt} />
       <script type="application/ld+json">{JSON.stringify(schema)}</script>
       <BlogPostTemplate post={post} />
     </>
